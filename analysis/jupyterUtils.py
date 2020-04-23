3058dd9d9998e6a85e958b00480b1563f8228679
Andreas Hoecherl <andreas.hoecherl@d-fine.de>
2020-04-23 23:08:01 +0200

    integrating Latex

--- analysis/jupyterUtils.py
+++ analysis/jupyterUtils.py
@@ -1,9 +1,10 @@
 from traitlets.config import Config
 import nbformat as nbf
-from nbconvert.exporters import HTMLExporter, LatexExporter
+from nbconvert.exporters import LatexExporter
 from nbconvert.writers import FilesWriter
 
-targetdirectory = r'C:\Oxford\Master Thesis\Allocation Thesis\SA_CCR_Allocation\LaTeX\JupyterNotebooks'
+targetDirectoryFull = r'C:\Oxford\Master Thesis\Allocation Thesis\SA_CCR_Allocation\LaTeX\JupyterNotebooksFull'
+targetDirectoryCore = r'C:\Oxford\Master Thesis\Allocation Thesis\SA_CCR_Allocation\LaTeX\JupyterNotebooksCore'
 
 def export(filename):
 
@@ -18,5 +19,26 @@ def export(filename):
     c.LatexExporter.preprocessors = ['nbconvert.preprocessors.TagRemovePreprocessor']
 
     writer = FilesWriter()
-    writer.build_directory = targetdirectory
-    writer.write(*LatexExporter(config=c).from_filename(filename), notebook_name=filename[:-6])
+    writer.build_directory = targetDirectoryFull
+    test = writer.write(*LatexExporter(config=c).from_filename(filename), notebook_name=filename[:-6])
+
+    createdFile = targetDirectoryFull+'\\' +filename[:-6] + '.tex'
+    toBeCreatedFile = targetDirectoryCore+'\\' +filename[:-6] + '.tex'
+
+    tag = 'maketitle'
+    lines_to_write = []
+    tag_found = False
+
+    with open(createdFile, encoding="utf8") as in_file:
+        for line in in_file:
+            if tag in line:
+                tag_found = True
+            elif tag_found:
+                lines_to_write.append(line)
+
+    lines_to_write = lines_to_write[1:-5]
+    with open(toBeCreatedFile, 'w+', encoding="utf8") as out_file:
+        out_file.writelines(lines_to_write)
+
+
+
